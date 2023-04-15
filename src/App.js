b318fd0cd9932d843f77b26a5498ad07ddbcad6b
serrasabah <serrasabah@gmail.com>
2023-04-15 20:38:38 +0300

    toastify eklendi

--- src/App.js
+++ src/App.js
@@ -2,15 +2,33 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import SignIn from "./sign/SignIn";
 import MainPageForStudent from "./StudentPages/MainPageForStudent";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<SignIn />} />
-        <Route element={<MainPageForStudent />} path={"/MainPageForStudent"} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<SignIn />} />
+          <Route
+            element={<MainPageForStudent />}
+            path={"/MainPageForStudent"}
+          />
+        </Routes>
+      </BrowserRouter>
+    </>
   );
 }
 
